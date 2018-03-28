decee4fce7882c122ca09c784c6b5e3988eb7039
derek chen <derek.g.chen@gmail.com>
2018-03-28 15:23:50 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,11 @@
 def nyc_pigeon_organizer(data)
   sorted_data ={}
+  temp = []
 
-
+  data.each do |key,val|
+    val.each do |attrbute|
+      temp << attrbute
+    end #val
+  end #data
+  temp
 end
